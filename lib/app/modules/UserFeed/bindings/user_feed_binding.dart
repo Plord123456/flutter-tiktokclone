281490f63ebf4804bc5e95e891f3f901f6917fa5
pham phu binh <phamphubinh3@gmail.com>
2025-06-20 16:05:31 +0700

    f

--- lib/app/modules/UserFeed/bindings/user_feed_binding.dart
+++ lib/app/modules/UserFeed/bindings/user_feed_binding.dart
@@ -5,10 +5,11 @@ import '../controllers/user_feed_controller.dart';
 class UserFeedBinding extends Bindings {
   @override
   void dependencies() {
-    final Map<String, dynamic> args = Get.arguments;
+    final Map<String, dynamic> args = Get.arguments is Map<String, dynamic>
+        ? Get.arguments as Map<String, dynamic>
+        : {};
 
     Get.lazyPut<UserFeedController>(() => UserFeedController(
-
       initialVideos: args['initialVideos'],
       initialIndex: args['initialIndex'],
     ));
